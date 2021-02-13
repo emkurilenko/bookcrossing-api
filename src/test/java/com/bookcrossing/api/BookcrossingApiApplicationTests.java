f2fc40ea85d7c8602cafddd5d57166b05ec090c7
Eugene Kurilenko <kurilenko.e.m@gmail.com>
2021-02-13 21:47:57 +0300

    initial settings

---
+++ src/test/java/com/bookcrossing/api/BookcrossingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookcrossing.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookcrossingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
